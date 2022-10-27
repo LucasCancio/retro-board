29f632a840ad3a2799ba43574194cced6f49687d
LucasCancio <lucas.cancio7@gmail.com>
2022-10-27 00:13:59 -0300

    Adicionando DialogModule em Header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -5,20 +5,17 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { DialogModule } from '../components/dialog/dialog.module';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    DialogModule,
   ],
-  exports: [
-    HeaderComponent
-  ]
+  exports: [HeaderComponent],
 })
-export class HeaderModule { }
+export class HeaderModule {}
